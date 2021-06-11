590f2700a3a12e989776389da74a66d4953b6755
BenCosfordHMRC <21200285+BenCosfordHMRC@users.noreply.github.com>
2021-06-11 16:38:53 +0100

    bumped up dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -5,7 +5,7 @@ object AppDependencies {
 
   val compile = Seq(
     "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.3.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.69.0-play-28",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.71.0-play-28",
     "uk.gov.hmrc"             %% "govuk-template"             % "5.68.0-play-27",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.50.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.2"
