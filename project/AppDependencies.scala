05cc2582fb1380e300c4f0baa24b92a27dbbd028
Robert Webb <28566263+robert-webb-hmrc@users.noreply.github.com>
2021-07-08 14:23:29 +0100

    SASS-879: Added HTTPOnly Cookie flag

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,15 +4,15 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.6.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.80.0-play-28",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.7.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.81.0-play-28",
     "uk.gov.hmrc"             %% "govuk-template"             % "5.69.0-play-28",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.50.0",
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.12.2"
+    "com.fasterxml.jackson.module" %% "jackson-module-scala"  % "2.12.2"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.3.0"                 % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.7.0"                 % Test,
     "org.scalatest"           %% "scalatest"                % "3.2.9"                 % Test,
     "org.jsoup"               %  "jsoup"                    % "1.13.1"                % Test,
     "com.typesafe.play"       %% "play-test"                % current                 % Test,
