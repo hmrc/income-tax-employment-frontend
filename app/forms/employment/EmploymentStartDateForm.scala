9f87df6ab9bd8a0fa11ca8dfe77ec9aa039f9e59
Joey Moore <22170468+joeyymoore@users.noreply.github.com>
2021-08-10 09:53:15 +0100

    Sass 1055 (#66)
    * Added start date page and Input Date helper
    
    * updated tests, and fixed error displays
    
    * updated dependencies
    
    * addressing code review
    
    * addressing code review

---
+++ app/forms/employment/EmploymentStartDateForm.scala
@@ -0,0 +1,68 @@
+/*
+ * Copyright 2021 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package forms.employment
+
+import forms.validation.mappings.MappingUtil.trimmedText
+import forms.validation.utils.ConstraintUtil.ConstraintUtil
+import models.employment.EmploymentDate
+import play.api.data.{Form, FormError}
+import play.api.data.Forms.mapping
+import play.api.data.validation.Constraint
+import play.api.data.validation.Constraints._
+import play.api.mvc.Results.BadRequest
+
+import java.time.LocalDate
+import scala.concurrent.Future
+import scala.util.Try
+
+object EmploymentStartDateForm {
+
+  val startDateDay: String = "amount-day"
+  val startDateMonth: String = "amount-month"
+  val startDateYear: String = "amount-year"
+
+  def dayEmpty: Constraint[String] = nonEmpty("employment.employmentStartDate.error.incompleteDay")
+  def monthEmpty: Constraint[String] = nonEmpty("employment.employmentStartDate.error.incompleteMonth")
+  def yearEmpty: Constraint[String] = nonEmpty("employment.employmentStartDate.error.incompleteYear")
+  def isDayNumeric: Constraint[String] = pattern("\\d+".r, error = "employment.employmentStartDate.error.incorrectDay" )
+  def isMonthNumeric: Constraint[String] = pattern("\\d+".r, error = "employment.employmentStartDate.error.incorrectMonth" )
+  def isYearNumeric: Constraint[String] = pattern("\\d+".r, error = "employment.employmentStartDate.error.incorrectYear" )
+
+  def employmentStartDateForm: Form[EmploymentDate] = Form(
+    mapping(
+      startDateDay -> trimmedText.verifying(dayEmpty andThen isDayNumeric) ,
+      startDateMonth -> trimmedText.verifying(monthEmpty andThen isMonthNumeric),
+      startDateYear -> trimmedText.verifying(yearEmpty andThen isYearNumeric)
+    )(EmploymentDate.apply)(EmploymentDate.unapply)
+  )
+
+  def fifthAprilDate(taxYear: Int): LocalDate = LocalDate.of(taxYear, 4, 5)
+
+  def verifyNewDate(date: EmploymentDate, taxYear: Int): Either[FormError, LocalDate] = {
+    val newDate: Either[Throwable, LocalDate] = Try(LocalDate.of(date.amountYear.toInt, date.amountMonth.toInt, date.amountDay.toInt)).toEither
+    newDate match {
+      case Right(date) =>
+        (date.isAfter(LocalDate.now()), date.isBefore(fifthAprilDate(taxYear))) match {
+          case (true, _) => Left(FormError("invalidFormat", "employment.employmentStartDate.error.notInPast"))
+          case (_, false) => Left(FormError("invalidFormat", "employment.employmentStartDate.error.tooRecent", Seq(taxYear.toString)))
+          case _ => Right(date)
+        }
+      case Left(_) =>
+        Left(FormError("invalidFormat", "employment.employmentStartDate.error.incorrect"))
+    }
+  }
+}
--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -4,15 +4,15 @@ import sbt._
 object AppDependencies {
 
   val compile = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.8.0",
-    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.87.0-play-28",
+    "uk.gov.hmrc"             %% "bootstrap-frontend-play-28" % "5.10.0",
+    "uk.gov.hmrc"             %% "play-frontend-hmrc"         % "0.90.0-play-28",
     "uk.gov.hmrc"             %% "govuk-template"             % "5.69.0-play-28",
     "uk.gov.hmrc.mongo"       %% "hmrc-mongo-play-28"         % "0.52.0",
     "com.fasterxml.jackson.module" %% "jackson-module-scala"  % "2.12.2"
   )
 
   val test = Seq(
-    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.8.0"                 % Test,
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % "5.10.0"                 % Test,
     "org.scalatest"           %% "scalatest"                % "3.2.9"                 % Test,
     "org.jsoup"               %  "jsoup"                    % "1.13.1"                % Test,
     "com.typesafe.play"       %% "play-test"                % current                 % Test,
