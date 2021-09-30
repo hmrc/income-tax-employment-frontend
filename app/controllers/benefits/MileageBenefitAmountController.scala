c338723eea79084febadf077a2a0f50ba1b44d6c
BenCosfordHMRC <21200285+BenCosfordHMRC@users.noreply.github.com>
2021-09-30 11:01:50 +0100

    updated controller and form

--- app/controllers/benefits/MileageBenefitAmountController.scala
+++ app/controllers/benefits/MileageBenefitAmountController.scala
@@ -17,23 +17,24 @@
 package controllers.benefits
 
 import config.{AppConfig, ErrorHandler}
+import controllers.employment.routes.CheckYourBenefitsController
 import controllers.predicates.{AuthorisedAction, InYearAction}
+import forms.AmountForm
+import forms.FormUtils.fillForm
 import javax.inject.Inject
+import play.api.data.Form
 import play.api.i18n.I18nSupport
 import play.api.mvc.{Action, AnyContent, MessagesControllerComponents}
 import services.EmploymentSessionService
 import uk.gov.hmrc.play.bootstrap.frontend.controller.FrontendController
 import utils.{Clock, SessionHelper}
 import views.html.employment.EmployerPayAmountView
-import controllers.employment.routes.CheckYourBenefitsController
-import forms.AmountForm
-import play.api.data.Form
 
 import scala.concurrent.Future
 
 class MileageBenefitAmountController @Inject()(implicit val cc: MessagesControllerComponents,
                                                authAction: AuthorisedAction,
-                                               employerPayAmountView: EmployerPayAmountView,
+                                               mileageBenefitAmountView: EmployerPayAmountView,
                                                inYearAction: InYearAction,
                                                appConfig: AppConfig,
                                                employmentSessionService: EmploymentSessionService,
@@ -62,9 +63,8 @@ class MileageBenefitAmountController @Inject()(implicit val cc: MessagesControll
               }
             }
 
-            lazy val unfilledForm = buildForm(user.isAgent)
-
-            ???
+            Future.successful(Ok(mileageBenefitAmountView(taxYear, fillForm(buildForm(user.isAgent), priorAmount, cyaAmount),
+              cyaAmount, cya.employment.employmentDetails.employerName, employmentId)))
 
           case None => Future.successful(Redirect(CheckYourBenefitsController.show(taxYear, employmentId)))
         }
@@ -74,7 +74,33 @@ class MileageBenefitAmountController @Inject()(implicit val cc: MessagesControll
 
   def submit(taxYear: Int, employmentId: String): Action[AnyContent] = authAction.async { implicit user =>
     inYearAction.notInYear(taxYear) {
-      ???
+
+      val redirectUrl = CheckYourBenefitsController.show(taxYear, employmentId).url
+
+
+      employmentSessionService.getSessionDataAndReturnResult(taxYear, employmentId)(redirectUrl) { data =>
+        buildForm(user.isAgent).bindFromRequest().fold(
+          { formWithErrors =>
+
+            Future.successful(BadRequest(mileageBenefitAmountView(taxYear, formWithErrors,
+              data.employment.employmentBenefits.flatMap(_.carVanFuelModel.flatMap(_.mileage)), data.employment.employmentDetails.employerName, employmentId)))
+
+          },{
+            amount =>
+
+              val cya = data.employment
+              val benefits = cya.employmentBenefits
+              val carVanFuel = cya.employmentBenefits.flatMap(_.carVanFuelModel)
+
+              val updatedCyaModel = cya.copy(employmentBenefits = benefits.map(_.copy(carVanFuelModel = carVanFuel.map(_.copy(mileage = Some(amount))))))
+
+              employmentSessionService.createOrUpdateSessionData(employmentId, updatedCyaModel, taxYear,
+                isPriorSubmission = data.isPriorSubmission)(errorHandler.internalServerError()) {
+                Redirect(redirectUrl)
+              }
+          }
+        )
+      }
     }
   }
 }
