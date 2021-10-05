d30c90300806339e3fba6d686fad80ecc8739e16
BenCosfordHMRC <21200285+BenCosfordHMRC@users.noreply.github.com>
2021-10-05 16:51:07 +0100

    updated redirect service

--- app/models/employment/CarVanFuelModel.scala
+++ app/models/employment/CarVanFuelModel.scala
@@ -35,54 +35,76 @@ case class CarVanFuelModel(
                             mileageQuestion: Option[Boolean] = None,
                             mileage: Option[BigDecimal] = None
                           ){
-//  def isFinished(implicit taxYear: Int, employmentId: String): Option[Call] ={
-//
-//    carVanFuelQuestion match {
-//      case Some(true) => carSectionFinished && vanSectionFinished && mileageSectionFinished
-//      case Some(false) => None
-//      case None => false
-//    }
-//  }
-//
-//  def carSectionFinished(implicit taxYear: Int, employmentId: String): Option[Call] ={
-//    carQuestion match {
-//      case Some(true) => carFuelSectionFinished
-//      case Some(false) => None
-//      case None => false
-//    }
-//  }
-//
-//  def carFuelSectionFinished(implicit taxYear: Int, employmentId: String): Option[Call] = {
-//    carFuelQuestion match {
-//      case Some(true) => carFuel.isDefined
-//      case Some(false) => None
-//      case None => false
-//    }
-//  }
-//
-//  def vanSectionFinished(implicit taxYear: Int, employmentId: String): Option[Call] ={
-//    vanQuestion match {
-//      case Some(true) => vanFuelSectionFinished
-//      case Some(false) => None
-//      case None => false
-//    }
-//  }
-//
-//  def vanFuelSectionFinished(implicit taxYear: Int, employmentId: String): Option[Call] = {
-//    vanFuelQuestion match {
-//      case Some(true) => vanFuel.isDefined
-//      case Some(false) => None
-//      case None => false
-//    }
-//  }
-//
-//  def mileageSectionFinished(implicit taxYear: Int, employmentId: String): Option[Call] = {
-//    mileageQuestion match {
-//      case Some(true) => mileage.isDefined
-//      case Some(false) => None
-//      case None => Some(CheckYourBenefitsController.show(taxYear, employmentId)) // TODO Mileage yes no
-//    }
-//  }
+  def isFinished(implicit taxYear: Int, employmentId: String): Option[Call] ={
+
+    carVanFuelQuestion match {
+      case Some(true) =>
+        (fullCarSectionFinished,fullVanSectionFinished,mileageSectionFinished) match {
+          case (call@Some(_), _, _) => call
+          case (_, call@Some(_), _) => call
+          case (_, _, call@Some(_)) => call
+          case _ => None
+        }
+      case Some(false) => None
+      case None => Some(CarVanFuelBenefitsController.show(taxYear, employmentId))
+    }
+  }
+
+  def fullCarSectionFinished(implicit taxYear: Int, employmentId: String): Option[Call] ={
+    (carSectionFinished, carFuelSectionFinished) match {
+      case (call@Some(_), _) => call
+      case (_, call@Some(_)) => call
+      case _ => None
+    }
+  }
+
+  def carSectionFinished(implicit taxYear: Int, employmentId: String): Option[Call] ={
+    carQuestion match {
+      case Some(true) => if(car.isDefined) None else Some(CompanyCarBenefitsController.show(taxYear, employmentId)) // TODO Car amount
+      case Some(false) => None
+      case None => Some(CompanyCarBenefitsController.show(taxYear, employmentId))
+    }
+  }
+
+  def carFuelSectionFinished(implicit taxYear: Int, employmentId: String): Option[Call] = {
+    carFuelQuestion match {
+      case Some(true) => if(carFuel.isDefined) None else Some(CompanyCarFuelBenefitsController.show(taxYear, employmentId)) // TODO Car fuel amount
+      case Some(false) => None
+      case None => Some(CompanyCarFuelBenefitsController.show(taxYear, employmentId))
+    }
+  }
+
+  def fullVanSectionFinished(implicit taxYear: Int, employmentId: String): Option[Call] ={
+    (vanSectionFinished, vanFuelSectionFinished) match {
+      case (call@Some(_), _) => call
+      case (_, call@Some(_)) => call
+      case _ => None
+    }
+  }
+
+  def vanSectionFinished(implicit taxYear: Int, employmentId: String): Option[Call] ={
+    vanQuestion match {
+      case Some(true) => if(van.isDefined) None else Some(CompanyVanBenefitsController.show(taxYear, employmentId)) // TODO Van amount
+      case Some(false) => None
+      case None => Some(CompanyVanBenefitsController.show(taxYear, employmentId))
+    }
+  }
+
+  def vanFuelSectionFinished(implicit taxYear: Int, employmentId: String): Option[Call] = {
+    vanFuelQuestion match {
+      case Some(true) => if(vanFuel.isDefined) None else Some(CompanyVanBenefitsController.show(taxYear, employmentId)) // TODO Van fuel amount
+      case Some(false) => None
+      case None => Some(CompanyVanBenefitsController.show(taxYear, employmentId)) // TODO Van fuel yes no
+    }
+  }
+
+  def mileageSectionFinished(implicit taxYear: Int, employmentId: String): Option[Call] = {
+    mileageQuestion match {
+      case Some(true) => if(mileage.isDefined) None else Some(MileageBenefitAmountController.show(taxYear,employmentId))
+      case Some(false) => None
+      case None => Some(CheckYourBenefitsController.show(taxYear, employmentId)) // TODO Mileage yes no
+    }
+  }
 }
 
 object CarVanFuelModel {
