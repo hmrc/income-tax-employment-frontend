e4cb3692c311d4fd3da51060abf247b507b701f6
John Kelly <60345872+jhkldev@users.noreply.github.com>
2021-12-13 11:14:44 +0000

    SASS-1932: Update employment FE to specify user agent for NRS submission

--- test/services/NrsServiceSpec.scala
+++ test/services/NrsServiceSpec.scala
@@ -20,6 +20,8 @@ import connectors.NrsConnector
 import connectors.httpParsers.NrsSubmissionHttpParser.NrsSubmissionResponse
 import models.employment.{DecodedCreateNewEmploymentDetailsPayload, DecodedNewEmploymentData, DecodedPriorEmploymentInfo}
 import play.api.libs.json.{JsString, Writes}
+import play.api.mvc.Request
+import play.api.test.FakeRequest
 import uk.gov.hmrc.http.HeaderCarrier
 import utils.UnitTest
 
@@ -53,7 +55,7 @@ class NrsServiceSpec extends UnitTest {
 
   ".postNrsConnector" when {
 
-    "there is a true client ip and port" should {
+    "there is user-agent, true client ip and port" should {
 
       "return the connector response" in {
 
@@ -62,24 +64,28 @@ class NrsServiceSpec extends UnitTest {
         val headerCarrierWithTrueClientDetails = headerCarrierWithSession.copy(trueClientIp = Some("127.0.0.1"), trueClientPort = Some("80"))
 
         (connector.postNrsConnector(_: String, _: DecodedCreateNewEmploymentDetailsPayload)(_: HeaderCarrier, _: Writes[DecodedCreateNewEmploymentDetailsPayload]))
-          .expects(nino, payloadModel, headerCarrierWithTrueClientDetails.withExtraHeaders("mtditid" -> mtditid, "clientIP" -> "127.0.0.1", "clientPort" -> "80"), writesObject)
+          .expects(nino, payloadModel, headerCarrierWithTrueClientDetails.withExtraHeaders("mtditid" -> mtditid, "User-Agent" -> "income-tax-employment-frontend", "True-User-Agent" -> "Firefox v4.4543 / Android v3.42 / IOS v134.32", "clientIP" -> "127.0.0.1", "clientPort" -> "80"), writesObject)
           .returning(Future.successful(expectedResult))
 
-        val result = await(service.submit(nino, payloadModel, mtditid)(headerCarrierWithTrueClientDetails, writesObject))
+        val request = FakeRequest().withHeaders("User-Agent" -> "Firefox v4.4543 / Android v3.42 / IOS v134.32")
+
+        val result = await(service.submit(nino, payloadModel, mtditid)(request, headerCarrierWithTrueClientDetails, writesObject))
 
         result shouldBe expectedResult
       }
     }
 
-    "there isn't a true client ip and port" should {
+    "there isn't user-agent, true client ip and port" should {
 
       "return the connector response" in {
 
         val expectedResult: NrsSubmissionResponse = Right()
 
         (connector.postNrsConnector(_: String, _: DecodedCreateNewEmploymentDetailsPayload)(_: HeaderCarrier, _: Writes[DecodedCreateNewEmploymentDetailsPayload]))
-          .expects(nino, payloadModel, headerCarrierWithSession.withExtraHeaders("mtditid" -> mtditid), writesObject)
+          .expects(nino, payloadModel, headerCarrierWithSession.withExtraHeaders("mtditid" -> mtditid, "User-Agent" -> "income-tax-employment-frontend", "True-User-Agent" -> "No user agent provided"), writesObject)
           .returning(Future.successful(expectedResult))
+
+        val request: Request[_] = FakeRequest()
 
         val result = await(service.submit(nino, payloadModel, mtditid))
 
