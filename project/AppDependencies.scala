b0569cdc1390359361aa6ea24bdfe9a09e7bbb58
Duane Miles <29195796+TheDeadlyPianist@users.noreply.github.com>
2022-02-01 13:48:05 +0000

    [DM] [SASS-2143] Removed deprecated dependency (#289)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2022 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 import play.core.PlayVersion.current
 import sbt._
 
@@ -6,7 +22,6 @@ object AppDependencies {
   val compile = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-28" % "5.20.0",
     "uk.gov.hmrc"                   %% "play-frontend-hmrc"         % "2.0.0-play-28",
-    "uk.gov.hmrc"                   %% "govuk-template"             % "5.74.0-play-28",
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-28"         % "0.59.0",
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.12.2"
   )
