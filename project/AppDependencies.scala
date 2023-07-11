9e8242df6419307dc5e3a3fc9a2177a388947a4d
Nicole Avison <nicole.avison@digital.hmrc.gov.uk>
2023-07-11 12:42:17 +0100

    tidying

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -23,23 +23,23 @@ object AppDependencies {
   private val mongoPlayVersion = "1.1.0"
 
   val compile: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootstrapFrontendPlay28Version,
-    "uk.gov.hmrc" %% "play-frontend-hmrc" % "7.3.0-play-28",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-play-28" % mongoPlayVersion,
-    "com.fasterxml.jackson.module" %% "jackson-module-scala" % "2.14.2"
+    "uk.gov.hmrc"                   %% "bootstrap-frontend-play-28" % bootstrapFrontendPlay28Version,
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc"         % "7.3.0-play-28",
+    "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-28"         % mongoPlayVersion,
+    "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.14.2"
   )
 
   val test: Seq[ModuleID] = Seq(
-    "uk.gov.hmrc" %% "bootstrap-test-play-28" % bootstrapFrontendPlay28Version % Test,
-    "org.scalatest" %% "scalatest" % "3.2.15" % Test,
-    "org.scalatestplus" %% "mockito-3-4" % "3.2.10.0" % Test,
-    "org.mockito" %% "mockito-scala" % "1.16.42" % Test,
-    "org.jsoup" % "jsoup" % "1.15.4" % Test,
-    "com.typesafe.play" %% "play-test" % current % Test,
-    "org.scalatestplus.play" %% "scalatestplus-play" % "5.1.0" % "test, it",
-    "com.github.tomakehurst" % "wiremock-jre8" % "2.35.0" % "test, it",
-    "org.scalamock" %% "scalamock" % "5.2.0" % Test,
-    "com.vladsch.flexmark" % "flexmark-all" % "0.64.0" % "test, it",
-    "uk.gov.hmrc.mongo" %% "hmrc-mongo-test-play-28" % mongoPlayVersion % "test, it"
+    "uk.gov.hmrc"             %% "bootstrap-test-play-28"   % bootstrapFrontendPlay28Version % Test,
+    "org.scalatest"           %% "scalatest"                % "3.2.15" % Test,
+    "org.scalatestplus"       %% "mockito-3-4"              % "3.2.10.0" % Test,
+    "org.mockito"             %% "mockito-scala"            % "1.16.42" % Test,
+    "org.jsoup"               % "jsoup"                     % "1.15.4" % Test,
+    "com.typesafe.play"       %% "play-test"                % current % Test,
+    "org.scalatestplus.play"  %% "scalatestplus-play"       % "5.1.0" % "test, it",
+    "com.github.tomakehurst"  % "wiremock-jre8"             % "2.35.0" % "test, it",
+    "org.scalamock"           %% "scalamock"                % "5.2.0" % Test,
+    "com.vladsch.flexmark"    % "flexmark-all"              % "0.64.0" % "test, it",
+    "uk.gov.hmrc.mongo"       %% "hmrc-mongo-test-play-28"  % mongoPlayVersion % "test, it"
   )
 }
