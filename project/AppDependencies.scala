6ae7d3cc166ca7f76c34e5eb5fb6eebdd89556f8
kSiadat <49532979+kSiadat@users.noreply.github.com>
2026-01-05 14:36:38 +0000

    MIPR-2300: dependency updates

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -18,7 +18,7 @@ import sbt.*
 
 object AppDependencies {
 
-  private val bootstrapFrontendPlay30Version = "10.4.0"
+  private val bootstrapFrontendPlay30Version = "10.5.0"
   private val mongoPlayVersion = "2.11.0"
 
   val jacksonAndPlayExclusions: Seq[InclusionRule] = Seq(
@@ -31,7 +31,7 @@ object AppDependencies {
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"                   %% "bootstrap-frontend-play-30" % bootstrapFrontendPlay30Version,
-    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30" % "12.22.0",
+    "uk.gov.hmrc"                   %% "play-frontend-hmrc-play-30" % "12.25.0",
     "uk.gov.hmrc.mongo"             %% "hmrc-mongo-play-30"         % mongoPlayVersion,
     "com.fasterxml.jackson.module"  %% "jackson-module-scala"       % "2.20.1",
     "com.beachape"                  %% "enumeratum"                 % "1.9.0",
@@ -44,7 +44,7 @@ object AppDependencies {
     "org.scalatest"           %% "scalatest"                % "3.2.19",
     "org.scalatestplus"       %% "mockito-3-4"              % "3.2.10.0",
     "org.mockito"             %% "mockito-scala"            % "2.0.0",
-    "org.jsoup"               %  "jsoup"                    % "1.21.2",
+    "org.jsoup"               %  "jsoup"                    % "1.22.1",
     "com.github.tomakehurst"  %  "wiremock-jre8-standalone" % "3.0.1",
     "org.scalamock"           %% "scalamock"                % "7.5.2",
     "com.vladsch.flexmark"    %  "flexmark-all"             % "0.64.8",
